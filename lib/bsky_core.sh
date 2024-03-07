03e2c100c78e9f984a3cd810f7de0064addc6f5c
bills-appworks <bills.appworks@gmail.com>
2024-03-07 12:58:31 +0900

    * license description add to files

--- lib/bsky_core.sh
+++ lib/bsky_core.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+# Bluesky in the shell
+# A Bluesky CLI (Command Line Interface) implemented with shell script
+# Author Bluesky:@billsbs.bills-appworks.net
+# 
+# Copyright (c) 2024 bills-appworks
+# This software is released under the MIT License.
+# http://opensource.org/licenses/mit-license.php
 FILE_DIR=`dirname "$0"`
 FILE_DIR=`(cd "${FILE_DIR}" && pwd)`
 . "${TOOLS_ROOT_DIR}/lib/util.sh"
