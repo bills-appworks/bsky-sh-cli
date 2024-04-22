44378b3104309b196c3ed869d259dc5a71959be0
bills-appworks <bills.appworks@gmail.com>
2024-04-22 11:14:03 +0900

    * implement getProfiles API
    * implement array parameters at proxy.sh

--- lib/api/proxy.sh
+++ lib/api/proxy.sh
@@ -41,6 +41,28 @@ build_query_parameter()
   debug 'build_query_parameter' 'END'
 }
 
+build_array_parameters()
+{
+  STACK="$1"
+  QUERY_KEY="$2"
+  shift
+  shift
+  QUERY_VALUES="$@"
+
+  debug 'build_array_parametres' 'START'
+  debug 'build_array_parametres' "STACK:${STACK}"
+  debug 'build_array_parametres' "QUERY_KEY:${QUERY_KEY}"
+  debug 'build_array_parametres' "QUERY_VALUES:${QUERY_VALUES}"
+
+  for QUERY_VALUE in $QUERY_VALUES
+  do
+    STACK=`build_query_parameter "${STACK}" "${QUERY_KEY}" "${QUERY_VALUE}"`
+  done
+  _p "${STACK}"
+
+  debug 'build_array_parametres' 'END'
+}
+
 create_authorization_header()
 {
   BEARER="$1"
@@ -50,6 +72,28 @@ create_authorization_header()
   _p "${HEADER_AUTHORIZATION_PREFIX} ${BEARER}"
 
   debug 'create_authorization_header' 'END'
+}
+
+api_get_raw_queries()
+{
+  ENDPOINT="$1"
+  BEARER="$2"
+  RAW_QUERIES="$3"
+
+  debug 'api_get_raw_queries' 'START'
+  debug 'api_get_raw_queries' "ENDPOINT:${ENDPOINT}"
+  debug 'api_get_raw_queries' "RAW_QUERIES:${RAW_QUERIES}"
+
+  if [ -z "${BEARER}" ]
+  then
+    read_session_file
+    BEARER="${SESSION_ACCESS_JWT}"
+  fi
+  HEADER_AUTHORIZATION=`create_authorization_header "${BEARER}"`
+  debug_single 'api_get_raw_queries'
+  curl -s -X GET "${ENDPOINT_BASE_URL}${ENDPOINT}${RAW_QUERIES}" -H "${HEADER_ACCEPT}" -H "${HEADER_AUTHORIZATION}" | tee "${BSKYSHCLI_DEBUG_SINGLE}"
+
+  debug 'api_get_raw_queries' 'END'
 }
 
 api_get()
@@ -78,14 +122,7 @@ api_get()
     shift
   done
 
-  if [ -z "${BEARER}" ]
-  then
-    read_session_file
-    BEARER="${SESSION_ACCESS_JWT}"
-  fi
-  HEADER_AUTHORIZATION=`create_authorization_header "${BEARER}"`
-  debug_single 'api_get'
-  curl -s -X GET "${ENDPOINT_BASE_URL}${ENDPOINT}${QUERY_PARAMETER}" -H "${HEADER_ACCEPT}" -H "${HEADER_AUTHORIZATION}" | tee "${BSKYSHCLI_DEBUG_SINGLE}"
+  api_get_raw_queries "${ENDPOINT}" "${BEARER}" "${QUERY_PARAMETER}"
 
   debug 'api_get' 'END'
 }
