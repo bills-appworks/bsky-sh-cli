fdb4229a08ff3b62d49bd9f59a4d810604ef4e60
bills-appworks <bills.appworks@gmail.com>
2024-04-22 14:33:38 +0900

    * support for pre-commit

--- shellcheck.sh
+++ shellcheck.sh
@@ -6,6 +6,9 @@
 # Copyright (c) 2024 bills-appworks
 # This software is released under the MIT License.
 # http://opensource.org/licenses/mit-license.php
+
+# recommend to use at pre-commit
+
 TARGETS='
   shellcheck.sh
   bin/bsky
@@ -14,11 +17,17 @@ TARGETS='
   lib/api/*
 '
 
+STATUS_MAX=0
 for TARGET in $TARGETS
 do
   echo "shellcheck >>>> ${TARGET}"
 # if checking SC1090:source to resource config file, set source-path to home directory
 ##  shellcheck --source-path="${HOME}" "$@" "${TARGET}"
   shellcheck "$@" "${TARGET}"
+  STATUS=$?
+  if [ $STATUS -gt $STATUS_MAX ]
+  then
+    STATUS_MAX=$STATUS
+  fi
 done
-
+exit $STATUS_MAX
