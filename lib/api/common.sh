5a82fb09bf02edfccf8805ff2cf6476a7efa9f26
bills-appworks <bills.appworks@gmail.com>
2024-04-28 10:04:54 +0900

    modify: local variable name change to lower case (common.sh)

--- lib/api/common.sh
+++ lib/api/common.sh
@@ -6,15 +6,15 @@
 # Copyright (c) 2024 bills-appworks
 # This software is released under the MIT License.
 # http://opensource.org/licenses/mit-license.php
-FILE_DIR=`dirname "$0"`
-FILE_DIR=`(cd "${FILE_DIR}" && pwd)`
+file_dir=`dirname "$0"`
+file_dir=`(cd "${file_dir}" && pwd)`
 
 if [ -z "${BSKYSHCLI_DEFINE_API_COMMON}" ]; then  # ifndef BSKYSHCLI_DEFINE_API_COMMON
 BSKYSHCLI_DEFINE_API_COMMON='defined'
 
 BSKYSHCLI_DEFAULT_RESOURCE_CONFIG_PATH="${HOME}/.bsky_sh_cli_rc"
 BSKYSHCLI_DEFAULT_TOOLS_WORK_DIR="${HOME}/.bsky_sh_cli"
-BSKYSHCLI_DEFAULT_TOOLS_ROOT_DIR="${FILE_DIR}/../.."
+BSKYSHCLI_DEFAULT_TOOLS_ROOT_DIR="${file_dir}/../.."
 BSKYSHCLI_DEFAULT_TOOLS_ROOT_DIR=`(cd "${BSKYSHCLI_DEFAULT_TOOLS_ROOT_DIR}" && pwd)`
 BSKYSHCLI_DEFAULT_LIB_PATH="${BSKYSHCLI_DEFAULT_TOOLS_ROOT_DIR}/lib"
 
@@ -57,24 +57,24 @@ then
 fi
 export BSKYSHCLI_API_PATH
 
-UTILITY_PATH="${BSKYSHCLI_LIB_PATH}/util.sh"
-if [ -r "${UTILITY_PATH}" ]
+utility_path="${BSKYSHCLI_LIB_PATH}/util.sh"
+if [ -r "${utility_path}" ]
 then
   # SC1090
   # shellcheck source=SCRIPTDIR/../util.sh
-  . "${UTILITY_PATH}"
+  . "${utility_path}"
 else
-  echo "tools internal configured file util.sh is not readable: ${UTILITY_PATH}"
+  echo "tools internal configured file util.sh is not readable: ${utility_path}"
   exit 1
 fi
-PROXY_PATH="${BSKYSHCLI_API_PATH}/proxy.sh"
-if [ -r "${PROXY_PATH}" ]
+proxy_path="${BSKYSHCLI_API_PATH}/proxy.sh"
+if [ -r "${proxy_path}" ]
 then
   # SC1090
   # shellcheck source=SCRIPTDIR/proxy.sh
-  . "${PROXY_PATH}"
+  . "${proxy_path}"
 else
-  echo "tools internal configured file proxy.sh is not readable: ${PROXY_PATH}"
+  echo "tools internal configured file proxy.sh is not readable: ${proxy_path}"
   exit 1
 fi
 
